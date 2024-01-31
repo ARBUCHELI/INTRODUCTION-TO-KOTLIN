28a72180570c7c68f76512da51407952196e9ad5
ARBUCHELI <radio_seattle@hotmail.com>
2024-01-31 09:49:58 +0300

    #introduction to kotlin - comments

---
+++ First-Kotlin-Project/src/Comments.kt
@@ -0,0 +1,5 @@
+fun main() {
+    //Code prints "Howdy, partner!" to the terminal.
+    /*Kotlin is a multipurpose programming language, used mainly in the development of mobile apps*/
+    println("Howdy, partner!")
+}
