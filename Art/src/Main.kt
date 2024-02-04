f9dc54b99daf6b9ca81249a57c3dc31631f5b8a6
ARBUCHELI <radio_seattle@hotmail.com>
2024-02-04 13:51:05 +0300

    #introduction to kotlin - project ascii art

---
+++ Art/src/Main.kt
@@ -0,0 +1,24 @@
+// Write your code below
+/*
+                .....
+            .           .
+          .     0   0     .
+            .............
+              .. ___ ..
+            .  .| B |. .
+          .    .......   .
+                .   .
+                .   .
+*/
+fun main () {
+    println("      ....      ")
+    println("  .           .")
+    println(".     0  0     .")
+    println("  .............")
+    println("    .. ___ ..")
+    println(" .   .| B | .  .")
+    println(".    ........   .")
+    println("_     .    .    _")
+    println("      .    .")
+    println("      .    .")
+}
