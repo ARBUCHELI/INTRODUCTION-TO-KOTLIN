4b770737c382fd38ad44dbd9e346bf6005a62ac7
ARBUCHELI <radio_seattle@hotmail.com>
2024-02-05 09:00:08 +0300

    #data-types-and-variables - declaring a mutable variable

---
+++ Data-Types-And-Variables/src/Date.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Write your code below ☀️
+    var todaysDate: String = "02/05/2024";
+    println(todaysDate);
+
+    var currentWeather: String;
+    currentWeather = "Cloudy";
+    println(currentWeather);
+}
